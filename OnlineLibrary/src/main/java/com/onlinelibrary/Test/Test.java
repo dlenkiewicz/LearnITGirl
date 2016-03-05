64e43a69e6549fb57e7c3a306eb015b7a64788c2
dlenkiewicz <dagmara.lenkiewicz@gmail.com>
2016-03-05 21:28:10 +0100

    Added Test class.

---
+++ OnlineLibrary/src/main/java/com/onlinelibrary/Test/Test.java
@@ -0,0 +1,36 @@
+package com.onlinelibrary.Test;
+
+import com.onlinelibrary.DAO.UserDAOImpl;
+import com.onlinelibrary.Model.User;
+
+public class Test {
+
+	public static void main(String[] args) {
+		UserDAOImpl dao = new UserDAOImpl();
+		
+		// Add new user
+        User user = new User();
+        user.setName("Carl");
+        user.setSurname("Smith");
+        user.setEmail("carl.smith@gmail.com");
+        user.setPassword("strongpsswd");
+        dao.save(user);
+
+        // Update user
+        user.setEmail("carl.smith.updated@gmail.com");
+        dao.update(user);
+
+        // Delete user
+        dao.delete(user);
+
+        // Get all users
+        for (User iter : dao.getAll()) {
+            System.out.println(iter);
+        }
+
+        // Get user by id
+        System.out.println(dao.get(2));
+
+		return;
+	}
+}
